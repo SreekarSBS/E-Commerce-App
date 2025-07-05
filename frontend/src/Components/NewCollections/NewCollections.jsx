5e395f7472858eb192ce1e4ddeb4644b14edb016
SreekarSBS <sbssreekar@gmail.com>
2025-07-05 22:56:39 +0530

    frontend fixes

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -6,7 +6,7 @@ const NewCollections = () => {
 const [new_collection,setNew_collection] = useState([]);
 
 useEffect(()=>{
-  fetch('http://localhost:4000/newcollections')
+  fetch('https://shopsphere-87rp.onrender.com/newcollections')
   .then((response)=>response.json())
   .then((data)=>setNew_collection(data));
 },[])
--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -7,7 +7,7 @@ const Popular = () => {
   const [popularProducts,setPopularProducts] = useState([]);
 
   useEffect(()=>{
-    fetch('http://localhost:4000/popularinwomen')
+    fetch('https://shopsphere-87rp.onrender.com/popularinwomen')
     .then((response)=> response.json())
     .then((data)=>setPopularProducts(data));
   },[])
